a6aaf3139baea47977aeec15ab60c0ec77c7cd48
Alexandr <digital-mag@ya.ru>
2021-09-28 10:40:34 +0300

    extension generator

--- lib/annotations.dart
+++ lib/annotations.dart
@@ -1,13 +1,15 @@
 class Constructor
 {
-    const Constructor();
+    const Constructor({this.name}): assert(name == null || name.length != 0);
+    final String? name;
 }
 
 const constructor = Constructor();
 
 
 class AutoProps{
-    const AutoProps()
+    const AutoProps({this.prefix = 's_'}) : assert(prefix.length != 0);
+    final String prefix;
 }
 
 const autoProps = AutoProps();
--- lib/builder.dart
+++ lib/builder.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:build/build.dart';
+import 'package:oop_extension_generator/annotations.dart';
 import 'package:source_gen/source_gen.dart';
 
 import 'package:analyzer/dart/element/element.dart';
+import 'package:logging/logging.dart';
 
 
 //Builder constructorGenerator(BuilderOptions options) => ConstructorGenerator();
@@ -10,20 +14,83 @@ import 'package:analyzer/dart/element/element.dart';
 Builder constructorGenerator(BuilderOptions options) => SharedPartBuilder([ConstructorGenerator()], 'constructor_generator');
 
 class ConstructorGenerator extends Generator {
+
+    final logger = Logger('ConstructorGenerator');
+
     @override
     String generate(LibraryReader library, BuildStep buildStep) {
 
-        print('-----------');
-        final sumNames = topLevelNumVariables(library)
-            .map((element) => element.name)
-            .join(' + ');
+        var result = '';
 
-        return '''num allSum() => $sumNames;''';
+        var constructorType = const TypeChecker.fromRuntime(Constructor);
+        var annotatedTypes = library.annotatedWith(
+            constructorType
+        );
+
+        var types = library.allElements
+            .whereType<ClassElement>()
+            .where((cls) => !cls.isEnum && !cls.isAbstract && !cls.isMixin)
+            .where((cls) => constructorType.annotationsOf(cls).isNotEmpty);
+
+        if (types.isEmpty) return '';
+
+        // ignore: avoid_print
+        print('start generation');
+
+        logger.info(types
+//            .where((cls) => annotatedTypes.contains(cls))
+//            .where((cls) => cls.constructors.isEmpty)
+//            .where((cls) => cls.constructors.isEmpty || annotatedTypes.contains(cls))
+            .map((e) => e.name)
+            .join(', ')
+        );
+
+        for (var cls in types) {
+
+            logger.info('<<<<<${cls.name}>>>>>>>');
+
+            var annotation = constructorType.annotationsOf(cls).first;
+//                var ctorName = annotation.getField('name')?.toStringValue() ?? '';
+//                var args = cls.fields.map((field) => 'this.' + field.name).join(', ');
+            var args = cls.fields.map((field) => '${field.type}? ${field.name}').join(', ');
+
+            var declaration = 'extension ${cls.name}Constructor on ${cls.name}{';
+//                var constructor = '    ${cls.name + (ctorName.isEmpty ? '': '.' + ctorName)}($args);';
+
+            var body = cls.fields.map((field) => ' '*8 + 'if (${field.name} != null) this.${field.name} = ${field.name};').join('\n');
+            var constructor = '    void _({$args}){\n$body\n    }';
+
+            var closing = '}';
+
+            result += declaration + constructor + closing;
+        }
+
+//        final sumNames = topLevelNumVariables(library)
+//            .map((element) => element.name)
+//            .join(' + ');
+
+        logger.info('result: ${result.length}');
+
+        return result;
     }
 }
 
-Iterable<TopLevelVariableElement> topLevelNumVariables(LibraryReader reader) =>
-    reader.allElements.whereType<TopLevelVariableElement>().where((element) =>
-    element.type.isDartCoreNum ||
-        element.type.isDartCoreInt ||
-        element.type.isDartCoreDouble);
+
+
+
+//class TodoReporterGenerator extends GeneratorForAnnotation<Constructor> {
+//    @override
+//    FutureOr<String> generateForAnnotatedElement(
+//        Element element, ConstantReader annotation, BuildStep buildStep) {
+//        return "// Hey! Annotation found!";
+//    }
+//}
+
+
+
+
+//Iterable<TopLevelVariableElement> topLevelNumVariables(LibraryReader reader) =>
+//    reader.allElements.whereType<TopLevelVariableElement>().where((element) =>
+//    element.type.isDartCoreNum ||
+//        element.type.isDartCoreInt ||
+//        element.type.isDartCoreDouble);
